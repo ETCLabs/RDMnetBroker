ceaa528036b82ef01c9479fcadb12913e89dac64
ETC Marketing <etcmarketing@ETCs-Mac-mini.local>
2022-08-02 12:17:47 -0500

    Preserve existing config during install

--- tools/install/macos/pre_install.sh
+++ tools/install/macos/pre_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -e
+
 if /bin/launchctl list "com.etcconnect.pkg.RDMnetBroker" &> /dev/null; then
   /bin/launchctl unload "/Library/LaunchDaemons/com.etcconnect.pkg.RDMnetBroker.plist"
+fi
+
+EXISTING_CONFIG=/usr/local/etc/RDMnetBroker/broker.conf
+CONFIG_BACKUP=/usr/local/etc/RDMnetBroker/broker.conf.backup
+if test -f "$EXISTING_CONFIG"; then
+  cp -p $EXISTING_CONFIG $CONFIG_BACKUP
 fi
