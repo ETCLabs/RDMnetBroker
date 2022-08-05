f0e7ca59b9db968baf81359b16494edb0556dfe0
ETC Marketing <etcmarketing@ETCs-Mac-mini.local>
2022-08-05 10:50:40 -0500

    Unload broker restarter first

--- tools/install/macos/pre_install.sh
+++ tools/install/macos/pre_install.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-if /bin/launchctl list "com.etcconnect.pkg.RDMnetBroker" &> /dev/null; then
-  /bin/launchctl unload "/Library/LaunchDaemons/com.etcconnect.pkg.RDMnetBroker.plist"
-fi
 if /bin/launchctl list "com.etcconnect.pkg.RDMnetBrokerRestarter" &> /dev/null; then
   /bin/launchctl unload "/Library/LaunchDaemons/com.etcconnect.pkg.RDMnetBrokerRestarter.plist"
+fi
+if /bin/launchctl list "com.etcconnect.pkg.RDMnetBroker" &> /dev/null; then
+  /bin/launchctl unload "/Library/LaunchDaemons/com.etcconnect.pkg.RDMnetBroker.plist"
 fi
 
 EXISTING_CONFIG=/usr/local/etc/RDMnetBroker/broker.conf
