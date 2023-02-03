d1416b305451ce725cf038c4c5d7317ae29a390a
ETC CI <noreply.ci@etcconnect.com>
2023-02-03 16:41:51 +0000

    [skip ci] Update version files for build 1.0.0.2

--- src/core/broker_version.h
+++ src/core/broker_version.h
@@ -38,7 +38,7 @@ struct BrokerVersion
   static constexpr int kVersionMajor = 1;
   static constexpr int kVersionMinor = 0;
   static constexpr int kVersionPatch = 0;
-  static constexpr int kVersionBuild = 1;
+  static constexpr int kVersionBuild = 2;
 
   static std::string VersionString();
   static std::string ProductNameString();
@@ -60,7 +60,7 @@ inline std::string BrokerVersion::ProductNameString()
 
 inline std::string BrokerVersion::BuildDateString()
 {
-  return "02.Feb.2023";
+  return "03.Feb.2023";
 }
 
 inline std::string BrokerVersion::CopyrightString()
