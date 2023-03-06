78ffded9bf79462fc71f59dd41f3889ebc42a44b
Christian Reese <Christian.Reese@etcconnect.com>
2023-03-06 13:03:53 -0600

    RDMNETBRKR-46 - Increase the network change detection cooldown time

--- src/macos/broker_service.cpp
+++ src/macos/broker_service.cpp
@@ -23,7 +23,7 @@
 #include <notify_keys.h>
 
 // The interval to wait before restarting (in case we get blasted with tons of notifications at once)
-static constexpr uint32_t kNetworkChangeCooldownMs = 5000u;
+static constexpr uint32_t kNetworkChangeCooldownMs = 20000u;
 
 static void InterfaceChangeCallback(CFNotificationCenterRef center,
                                     void*                   observer,
