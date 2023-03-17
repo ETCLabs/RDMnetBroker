e144d2801cf8ad04046863fc9a0aa50c374c4b1a
Christian Reese <Christian.Reese@etcconnect.com>
2023-03-17 16:35:32 -0500

    Correct multiline approach

--- tools/ci/index_and_publish_symbols.bat
+++ tools/ci/index_and_publish_symbols.bat
@@ -1,10 +1,10 @@
-python C:\tools\ci\Symbols\publish_symbols.py \
-      -n "RDMnetBrokerService" \
-      -v %NEW_BUILD_VERSION% \
-      -a %COMPILER_ARCH% \
-      -u https://artifactory.etcconnect.com:443 \
-      -r NET \
-      -k %RDMNETBRKR_ARTIFACTORY_API_KEY% \
-      --os windows \
-      -d .\symbols \
-      .\build\%CMAKE_INSTALL%\bin\RDMnetBrokerService.exe
+python C:\tools\ci\Symbols\publish_symbols.py^
+  -n "RDMnetBrokerService"^
+  -v %NEW_BUILD_VERSION%^
+  -a %COMPILER_ARCH%^
+  -u https://artifactory.etcconnect.com:443^
+  -r NET^
+  -k %RDMNETBRKR_ARTIFACTORY_API_KEY%^
+  --os windows^
+  -d .\symbols^
+  .\build\%CMAKE_INSTALL%\bin\RDMnetBrokerService.exe
