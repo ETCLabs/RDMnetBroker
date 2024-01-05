0c80675f0fee6c59cc4a1ee539fba4ec94443444
Christian Reese <Christian.Reese@etcconnect.com>
2024-01-05 13:03:08 -0600

    Try downloading cacert directly

--- tools/ci/index_and_publish_symbols.bat
+++ tools/ci/index_and_publish_symbols.bat
@@ -20,6 +20,8 @@ powershell C:\tools\ci\github-sourceindexer.ps1 -dbgToolsPath "%TEMP%\srcsrv" -g
   -sourcesRoot "%cd%" ^
   -ignoreUnknown -verifyLocalRepo
 
+curl -o cacert.pem http://etc.gitlab-pages.etcconnect.com/cacert/wildcard-cert-chain.pem
+
 python C:\tools\ci\Symbols\publish_symbols.py ^
   -n "RDMnetBrokerService" ^
   -v %NEW_BUILD_VERSION% ^
@@ -29,4 +31,5 @@ python C:\tools\ci\Symbols\publish_symbols.py ^
   -k %RDMNETBRKR_ARTIFACTORY_API_KEY% ^
   --os windows ^
   -d .\symbols ^
+  --cert .\cacert.pem
   .\build\%CMAKE_INSTALL%\bin\RDMnetBrokerService.exe
